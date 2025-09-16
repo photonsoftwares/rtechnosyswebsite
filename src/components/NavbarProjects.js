dd784976f77e3b53fe9753dda7802db9d94bf4c6
Aman Mishra <amanmishr99@gmail.com>
2025-09-16 15:16:46 +0530

    new web design

--- src/components/NavbarProjects.js
+++ src/components/NavbarProjects.js
@@ -38,10 +38,10 @@ const NavbarProjects = () => {
       {/* Centered Text */}
       <div className="relative z-10 flex flex-col items-center justify-center flex-grow">
         <span className="px-6 py-2 bg-gray-700 bg-opacity-80 text-white rounded-full text-sm">
-          Our Projects
+          Our Products
         </span>
         <h1 className="mt-4 text-3xl md:text-4xl font-bold text-white">
-          Our Projects
+          Our Products
         </h1>
       </div>
     </div>
